faa6df998cded3aa077593d7c5007fbfd9873cdf
wjsxorjs <wjsxorjs@naver.com>
2024-05-22 12:45:19 +0900

    JSP 20240522 0.5

--- Ex20240522/src/main/java/am/Ex5_Servlet.java
+++ Ex20240522/src/main/java/am/Ex5_Servlet.java
@@ -9,15 +9,15 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 /**
- * Servlet implementation class Ex4_Servlet
+ * Servlet implementation class Ex5_Servlet
  */
-public class Ex4_Servlet extends HttpServlet {
+public class Ex5_Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Ex4_Servlet() {
+    public Ex5_Servlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,33 +26,27 @@ public class Ex4_Servlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// 요청 시 한글 처리
+		
 		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
 		
-		// 응답 시 한글 처리
-		response.setContentType("text/html; charset=utf-8"); // MIME타입
+		String[] fname = request.getParameterValues("f_name");
 		
-		// 응답을 위한 스트림 준비
 		PrintWriter out = response.getWriter();
 		
-		// 요청(request)한 곳으로 부터 파라미터(인자) 받기
-		// msg라는 이름으로 전달되는 인자를 받자
-		String name = request.getParameter("m_name");
-		String[] hobbies = request.getParameterValues("m_hobby");
-
-		// 응답시작
-		out.println("<h2> 받은 이름:"+name+"</h2>");
-			if(hobbies != null) {
-			out.println("<h2> 취미: </h2>");
-			for(int i=0; i<hobbies.length; i++) {
-				out.println(hobbies[i]);
-				if(i<hobbies.length-1) {
-					out.println(",");
-				}
+		int count = 0;
+		
+		for(int i = 0; i<fname.length; i++) {
+			if(fname[i].trim().length()>0) {
+				out.println("<h2> 친구"+(count+1)+"의 이름: "+fname[i]+"</h2>");
+				count++;
 			}
-			
-			out.close();
 		}
+		
+		
+		out.println("친구"+count+"명");
+		
+		out.close();
 	}
 
 	/**
